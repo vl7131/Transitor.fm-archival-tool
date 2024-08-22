2020ecc8993fe4c13dca511446a17a965c300350
Vitus Larrieu <144076242+vl7131@users.noreply.github.com>
2024-08-22 17:32:37 -0400

    A simple archival tool

---
+++ downloadscript.py
@@ -0,0 +1,39 @@
+import requests
+import time
+
+# API key, show ID, and passed parameters (which we use to set the number of episodes we are downloading)
+api_key = "YOUR_API_KEY" # API key is in your account profile under API access
+show_id = "YOUR_SHOW_ID" # Show ID is the last thing in your address bar for a show ex. https://dashboard.transistor.fm/shows/show_id
+params = {'pagination[per]': 1} # Adjust this number for how many episodes you want to download
+headers = {"x-api-key": api_key,"Content-Type": "application/json"}
+
+# Get a list of episodes in a show, number of episodes is controlled by params
+api_response = requests.get(f"https://api.transistor.fm/v1/episodes?show_id={show_id}", params=params, headers=headers)
+
+# Checks if API call was successful
+if api_response.status_code == 200:
+    episodes = api_response.json()["data"]
+    time.sleep(1)
+
+    for episode in episodes:
+        # the following are used to name the file and grab the audio url
+        # you can use other attributes too, found here: https://developers.transistor.fm/#Episode
+        ep_number = episode["attributes"]["number"]
+        pub_date = episode["attributes"]["formatted_published_at"]
+        title = episode['attributes']['title']
+        audio_url = episode['attributes']['media_url']
+        print(f"Downloading: EP.{ep_number} '{title}' ({pub_date}).mp3")
+
+        # Download the audio file, wait to avoid rate limits
+        audio_response = requests.get(audio_url)
+        if audio_response.status_code == 200:
+            with open(f"EP.{ep_number} '{title}' ({pub_date}).mp3", "wb") as file:
+                file.write(audio_response.content)
+            time.sleep(1)
+        else: 
+            print(f"API access failed: {audio_response.status_code}")
+            break
+
+# Gives error message if API call fails
+else:
+        print(f"API access failed: {api_response.status_code}")
